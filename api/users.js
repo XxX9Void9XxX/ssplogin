178d747bdee2494be47c71a5f578e25ea3239eec
XxX9Void9XxX <brooksm@carbonschools.org>
2026-02-24 16:48:12 -0700

    Update users.js

--- api/users.js
+++ api/users.js
@@ -1,4 +1,10 @@
+import sqlite3 from "sqlite3";
+
+const db = new sqlite3.Database("./database.sqlite");
+
 export default function handler(req, res) {
-  const users = global.users;
-  res.json({ users, onlineCount: users.length });
+  if (req.method !== "GET") return res.status(405).json({ error: "Method not allowed" });
+  db.all("SELECT username, role, banned, coins FROM users", (err, rows) => {
+    res.json({ users: rows });
+  });
 }
